01560a88adeb5818e005cc08894606705ecf4da6
Chu Phong <man.nguyen.cit@gmail.com>
2023-11-10 06:08:45 +0700

    responsive slider

--- components/slider/index.jsx
+++ components/slider/index.jsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import classNames from "classnames";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, MenuSquare, XCircle } from "lucide-react";
 // import Image from "next/image";
 import Link from "next/link";
 
 // import banner1 from "@/assets/images/banner/banner1.jpg";
 import { useTrans } from "@/helper/chanLang";
+import { useOutsideLeftDrawderClick } from "@/helper/clickOutsideElement";
 
 import Banner from "../banner";
 
@@ -15,6 +16,14 @@ function Slider() {
   const data = useTrans("slideBar");
 
   const [categories, setCategories] = useState(data);
+
+  const [isOpenDrawderLeft, setIsOpenDrawderLeft] = useState(false);
+
+  const openDrawerLeft = useCallback(() => setIsOpenDrawderLeft(true), []);
+
+  const closeDrawerLeft = useCallback(() => setIsOpenDrawderLeft(false), []);
+
+  const leftDrawderRef = useOutsideLeftDrawderClick(openDrawerLeft, closeDrawerLeft, isOpenDrawderLeft);
 
   useEffect(() => {
     setCategories(data);
@@ -86,12 +95,89 @@ function Slider() {
         </ul>
       </div>
 
-      <div className="pt-[2.5rem] lg:pl-[2.81rem]">
+      <div className="relative pt-[2.5rem] lg:pl-[2.81rem]">
         <div className="grid grid-cols-12 bg-black">
           <div className="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-12">
             <Banner />
           </div>
         </div>
+
+        <button
+          ref={!isOpenDrawderLeft ? leftDrawderRef : null}
+          type="button"
+          className="lg:hidden absolute bottom-[-5rem] left-0"
+        >
+          <MenuSquare />
+        </button>
+      </div>
+
+      <div
+        ref={isOpenDrawderLeft ? leftDrawderRef : null}
+        className={classNames(
+          styles.left_drawder,
+          "fixed top-0 left-0 h-screen min-w-fit bg-white shadow-md z-[3]",
+          !isOpenDrawderLeft && "hidden",
+        )}
+      >
+        <button
+          type="button"
+          onClick={closeDrawerLeft}
+          className="hover:opacity-50 transition-opacity ease-in-out duration-300 absolute top-3 right-3"
+        >
+          <XCircle />
+        </button>
+
+        <ul className="text-black font-poppins gap-[1rem] pt-[5.5rem] pl-[1rem] text-[1rem] font-[400] leading[1.5rem] flex flex-col">
+          {categories?.map((item, index) => {
+            if (item?.child) {
+              return (
+                <div
+                  onMouseEnter={(event) => toggleFrameCategories(event, index)}
+                  onMouseLeave={(event) => toggleFrameCategories(event, index)}
+                  className="relative"
+                  key={item.name}
+                >
+                  <li className="hover:opacity-50 transition-opacity ease-in-out duration-300 max-h-[1.5rem] flex justify-between items-center cursor-pointer ">
+                    <span className="mr-[3.19rem] !whitespace-nowrap">{item.name}</span>
+
+                    <ChevronRight className="mr-[1rem]" />
+                  </li>
+
+                  <ul
+                    className={classNames(
+                      `${styles.custom_shadow} bg-white rounded-md absolute gap-[1rem] left-[1rem] top-[1.5rem] z-[2] w-full h-[20rem] px-2 py-3 flex flex-col`,
+                      !item?.isExtend && "hidden",
+                    )}
+                  >
+                    {item?.child?.map((child) => {
+                      return (
+                        <li
+                          key={child.name}
+                          className="hover:opacity-50 transition-opacity ease-in-out duration-300 flex items-center max-h-[1.5rem] !whitespace-nowrap"
+                        >
+                          <Link className="px-2 py-1 block w-full" href="./">
+                            {child.name}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              );
+            }
+
+            return (
+              <li
+                className="hover:opacity-50 transition-opacity ease-in-out duration-300 flex items-center max-h-[1.5rem]"
+                key={item.name}
+              >
+                <Link className="!whitespace-nowrap block w-full" href="./">
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -40,7 +40,7 @@ function AccountPage() {
         ref={isOpenDrawderLeft ? leftDrawderRef : null}
         className={classNames(
           styles.left_drawder,
-          "fixed top-[7rem] left-0 h-screen min-w-fit shadow-md bg-text-1 z-[2]",
+          "fixed top-[7rem] left-0 h-screen min-w-fit bg-white shadow-md z-[2]",
           !isOpenDrawderLeft && "hidden",
         )}
       >
