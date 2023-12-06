b9fdd32fa480a74ba745e1ea4124eac51a892f17
Man Nguyen <128272683+ChuPhongMCP@users.noreply.github.com>
2023-12-06 16:08:17 +0700

    update ui

--- pages/account/my_orders/[id].jsx
+++ pages/account/my_orders/[id].jsx
@@ -84,60 +84,67 @@ function OrderDetails(props) {
       )}
       <div className="container mt-[5rem]">
         <div className="min-w-[20rem] min-h-fit bg-text-1 shadow-md rounded-sm px-[2rem] py-[2rem] whitespace-nowrap">
-          <div className="py-[0.5rem] mb-[1rem] flex items-center justify-start">
-            <span className="pl-[0.5rem] min-w-[33.5rem] max-w-[33.5rem] font-inter text-[1rem] font-[700] leading-[1rem]">
-              Product
-            </span>
-            <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[700] leading-[1rem]">Quantity</span>
-            <span className="min-w-[7.5rem] max-w-[7.5rem] font-inter text-[1rem] font-[700] leading-[1rem]">
-              Discount
-            </span>
-            <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[700] leading-[1rem]">Price</span>
-            <span className="min-w-[8rem] max-w-[8rem] font-inter text-[1rem] font-[700] leading-[1rem]">
-              Sub Total
-            </span>
-          </div>
-          {orderDetail?.orderDetails?.map((item) => {
-            return (
-              <div key={item.productId} className="py-[0.5rem] flex items-center justify-start">
-                <Link
-                  title={item?.product?.name}
-                  className="flex items-center justify-start"
-                  href={`/${item.productId}`}
-                >
-                  <Image
-                    className="max-w-[4rem] max-h-[4rem] mr-[0.5rem]"
-                    src={item?.product?.image?.location}
-                    alt="..."
-                    width={1000}
-                    height={1000}
-                  />
+          <div className="cover_order_detail overflow-x-auto">
+            <div className="py-[0.5rem] mb-[1rem] flex items-center justify-start">
+              <span className="pl-[0.5rem] min-w-[14.5rem] max-w-[14.5rem] sm:min-w-[19.5rem] sm:max-w-[19.5rem] md:min-w-[24.5rem] md:max-w-[24.5rem] lg:min-w-[33.5rem] lg:max-w-[33.5rem] font-inter text-[1rem] font-[700] leading-[1rem]">
+                Product
+              </span>
+              <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[700] leading-[1rem]">
+                Quantity
+              </span>
+              <span className="min-w-[7.5rem] max-w-[7.5rem] font-inter text-[1rem] font-[700] leading-[1rem]">
+                Discount
+              </span>
+              <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[700] leading-[1rem]">
+                Price
+              </span>
+              <span className="min-w-[8rem] max-w-[8rem] font-inter text-[1rem] font-[700] leading-[1rem]">
+                Sub Total
+              </span>
+            </div>
 
-                  <span className="min-w-[29.5rem] max-w-[29.5rem] truncate font-inter text-[1rem] font-[400] leading-[1rem]">
-                    {item?.product?.name}
+            {orderDetail?.orderDetails?.map((item) => {
+              return (
+                <div key={item.productId} className="py-[0.5rem] flex items-center justify-start">
+                  <Link
+                    title={item?.product?.name}
+                    className="flex items-center justify-start"
+                    href={`/${item.productId}`}
+                  >
+                    <Image
+                      className="max-w-[4rem] max-h-[4rem] mr-[0.5rem]"
+                      src={item?.product?.image?.location}
+                      alt="..."
+                      width={1000}
+                      height={1000}
+                    />
+
+                    <span className="min-w-[10.5rem] max-w-[10.5rem] sm:min-w-[15.5rem] sm:max-w-[15.5rem] md:min-w-[20.5rem] md:max-w-[20.5rem] lg:min-w-[29.5rem] lg:max-w-[29.5rem] truncate font-inter text-[1rem] font-[400] leading-[1rem]">
+                      {item?.product?.name}
+                    </span>
+                  </Link>
+
+                  <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[400] leading-[1rem]">
+                    {item?.quantity}
                   </span>
-                </Link>
 
-                <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[400] leading-[1rem]">
-                  {item?.quantity}
-                </span>
+                  <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[400] leading-[1rem]">
+                    {item?.discount}%
+                  </span>
 
-                <span className="min-w-[7rem] max-w-[7rem] font-inter text-[1rem] font-[400] leading-[1rem]">
-                  {item?.discount}%
-                </span>
+                  <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[400] leading-[1rem]">
+                    ${parseFloat(item?.price).toFixed(2)}
+                  </span>
 
-                <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[400] leading-[1rem]">
-                  ${parseFloat(item?.price).toFixed(2)}
-                </span>
-
-                <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[400] leading-[1rem]">
-                  $
-                  {(parseFloat(item?.quantity) * (parseFloat(item?.price) * (100 - parseInt(item?.discount, 10)))) /
-                    100}
-                </span>
-              </div>
-            );
-          })}
+                  <span className="min-w-[10rem] max-w-[10rem] font-inter text-[1rem] font-[400] leading-[1rem]">
+                    $
+                    {(parseFloat(item?.quantity) * (parseFloat(item?.price) * (100 - parseInt(item?.discount, 10)))) /
+                      100}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
 
           <div className="flex flex-col items-start justify-center mt-[2rem]">
             <span className="font-inter text-[1rem] font-[500] leading-[2rem]">
